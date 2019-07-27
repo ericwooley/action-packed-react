b4a21af6c80514bcfa86c54fe575c29eccae60d4
Eric Wooley <ericwooley@gmail.com>
2019-07-27 13:40:20 -0700

    add documentation to actionPack

--- action-packed-react/createReducer.ts
+++ action-packed-react/createReducer.ts
@@ -15,6 +15,34 @@ export interface IActionPack<T, IState> {
   _type: string
 }
 
+/**
+ * Create action pack creates an a action creator which accepts a payload and
+ * returns a flux standard action and the action creator has a ._type, for
+ * easily checking the type that would be returned, as well as a _handler,
+ * which can be used to reduce a state of the expected type.
+ * @param type string to be used as the action type.
+ * @param handler handler, which should take a state, and standard action,
+ *   then return a modified state
+ * @see createReducerFromActionPack for combining actionPacks into a single
+ *   reducer
+ * @example
+ * const firstTodo {title: 'Some thing i need to do', done: false}
+ * type Todo = typeof firstTodo;
+ * const initialTodoState = [firstTodo];
+ * type TodoState = typeof initialTodoState;
+ * const addTodo = createActionPack<TodoState, Todo>(
+ *  'ADD_TODO',
+ *  (state, action) => [...state, action.payload]
+ *  );
+ * // addTodo._type // ADD_TODO
+ * const addTodoDemoAction = addTodo({title: 'Something else I need to do', done: true})
+ * // {type: 'ADD_TODO', payload: {title: 'Something else I need to do', done: true} }
+ * addTodo._handler(initialState, addTodoDemoAction)
+ * // [
+ * //   {title: 'Some thing i need to do', done: false},
+ * //   {title: 'Something else I need to do', done: true}
+ * // ]
+ */
 export const createActionPack = <TState, T>(type: string, handler: IHandler<T, TState>) => {
   const ac: IActionPack<T, TState> & any = (payload: T) => ({
     payload,
@@ -25,10 +53,35 @@ export const createActionPack = <TState, T>(type: string, handler: IHandler<T, T
   return ac as IActionPack<T, TState>
 }
 
+/**
+ * Create a reducer with actionCreators as a property, which uses the
+ * actionPack handlers to reduce state.
+ * @param initialState The initial state to be used by the reducer
+ * @param actionPacks Object of action packs whose handlers will be used to
+ *   to reduce the the state when the action packs type is dispatched
+ * @example
+ * const firstTodo {title: 'Some thing i need to do', done: false}
+ * type Todo = typeof firstTodo;
+ * const initialTodoState = [];
+ * const reducer = createReducerFromActionPack(initialTodoState, {
+ *   addTodo: createActionPack<TodoState, Todo>(
+ *     'ADD_TODO',
+ *     (state, action) => [...state, action.payload]
+ *   ),
+ *   removeTodo: createActionPack<TodoState, Todo>(
+ *     'REMOVE_TODO',
+ *     (state, action) => state.filter(todo => todo !== action.payload)
+ *   ),
+ * });
+ * let withFirstTodo = reducer(initialState, reducer.actionCreators.addTodo(firstTodo))
+ * // [{title: 'Some thing i need to do', done: false}]
+ * let withoutFirstTodo = reducer(withFirstTodo, reducer.actionCreators.removeTodo(firstTodo))
+ * // []
+ *
+ */
 export const createReducerFromActionPack = <T>(
   initialState: T,
-  actionPacks: {[key: string]: IActionPack<any, T>},
-  {  }: {} = {}
+  actionPacks: { [key: string]: IActionPack<any, T> }
 ): Reducer<T> & { actionCreators: typeof actionPacks } => {
   const handlerMap = Object.values(actionPacks).reduce(
     (hmap, handler) => {
