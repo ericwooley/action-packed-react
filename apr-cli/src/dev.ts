425bb95d994f2269ad6d1979b1a732531a590b8b
Eric Wooley <ericwooley@gmail.com>
2019-08-04 15:14:20 -0700

    add basic webpack config

--- apr-cli/src/dev.ts
+++ apr-cli/src/dev.ts
@@ -1,3 +1,12 @@
-module.exports = async function dev () {
-  console.log('start dev server')
-}
+import { spawnSync } from "child_process";
+import which from "which";
+import debug from "debug";
+const log = debug("apr:dev");
+module.exports = async function dev() {
+  const serverCommand = which.sync("webpack-dev-server");
+  const args = ["--config", "./webpack/webpack.config.js"]
+  log("starting server command:", serverCommand, 'with args', args);
+  spawnSync(which.sync("webpack-dev-server"), {
+    stdio: "inherit"
+  });
+};
