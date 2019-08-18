45c7252b6034399b177a02e88f09a50ce1c0a77c
Eric Wooley <ericwooley@gmail.com>
2019-08-18 10:30:55 -0700

    fix name validator, and fix no route on ui

--- apr-cli/_templates/nameQuestion.js
+++ apr-cli/_templates/nameQuestion.js
@@ -6,11 +6,11 @@ module.exports = (name) => ({
   message: 'Name your ' + name,
   validate: (result) => {
     if (changeCase.camel(result) !== result) {
-      return 'name must be camel case'
+      return 'name must be camel case '
     }
-    console.log('name is fine', changeCase.camel(result), result)
     return true
   },
-  format: changeCase.camel,
+  // scrapped for now.
+  // format: changeCase.camel,
   result: changeCase.camel
 })
--- apr-cli/_templates/ui/new/index.js
+++ apr-cli/_templates/ui/new/index.js
@@ -1,9 +1,7 @@
-const routeQuestion = require('../../routeQuestion')
 const promptArgs = require('../../promptArgsUtil')
 const nameQuestion = require('../../nameQuestion')
 
 const questions = [
-  routeQuestion,
   nameQuestion('ui component')
 ]
 module.exports = {
