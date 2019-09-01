5c43e942f95f71114f49192e595eabe99e413662
Eric Wooley <ericwooley@gmail.com>
2019-08-31 18:51:52 -0700

    don't test webpck server

--- apr-cli/src/test/0.initPlayground.test.ts
+++ apr-cli/src/test/0.initPlayground.test.ts
@@ -1,23 +1,12 @@
 import { spawnSync } from "child_process";
 import { snapshotPlayground } from "./utils/getAllSourceFiles";
-import chalk from "chalk";
 import axios from "axios";
 
-const warnAboutErrorCode = (code: number | null) => {
-  if (code) {
-    process.stderr.write(chalk.red(`\n\ninit playground exited code ${code}\n\n`));
-    process.exitCode = code;
-  }
-};
 beforeAll(() => {
   process.chdir("../");
   spawnSync("yarn", ["playground-init"], { stdio: "inherit" });
 });
 describe("setup", () => {
-  it("should start the dev server", async () => {
-    const result = await axios.get("http://localhost:8080");
-    expect(result.status).toEqual(200);
-  });
   it("should create the initial files", async () => {
     await snapshotPlayground();
   });
