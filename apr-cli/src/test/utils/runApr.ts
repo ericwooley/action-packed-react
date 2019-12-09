92f17194de8ae93874b332c07f09b9fca8668b46
Eric Wooley <ericwooley@gmail.com>
2019-12-08 21:51:06 -0800

    fix again

--- apr-cli/src/test/utils/runApr.ts
+++ apr-cli/src/test/utils/runApr.ts
@@ -3,24 +3,27 @@ import debug from "debug";
 import { cleanSourcePath } from "./cleanSourcePath";
 import { join } from "path";
 const log = debug("apr:runApr");
-export const runApr = (args: string[] | string, { snapshotOutput = false, autoLink = true } = {}) => {
+export const runApr = (
+  args: string[] | string,
+  { snapshotOutput = false, autoLink = true } = {}
+) => {
   if (typeof args === "string") {
     args = args.split(/\s/g);
   }
-  if(autoLink) {
-    args.push('--autoLink')
+  if (autoLink) {
+    args.push("--autoLink");
   }
   const prettyCommand = ["apr", ...args].join(" ");
   process.chdir(join(__dirname, "../../../../playground"));
   log("running: ", prettyCommand);
   const result = spawnSync("apr", args);
-  log("prettycommand result:", result, result.output.toString())
-  const output = cleanSourcePath( result.output.toString());
-  log("exit status for", prettyCommand, result.status);
-  log("output for", prettyCommand, "\n", output);
   if (result.status !== 0) {
     throw new Error("Unsuccessful apr command: " + ["apr", ...args].join(" "));
   }
+  log("prettycommand result:", {status: result.status}, result.output.toString());
+  const output = cleanSourcePath(result.output.toString());
+  log("exit status for", prettyCommand, result.status);
+  log("output for", prettyCommand, "\n", output);
   if (snapshotOutput) {
     expect(output).toMatchSnapshot(cleanSourcePath(prettyCommand));
   }
